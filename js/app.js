059240b2c21330a3d1521e83a1e3e31410206380
carloscazares-ai <carlos.cazares@cua.uam.mx>
2026-03-16 14:20:25 -0600

    agregando las funciones app.js

---
+++ js/app.js
@@ -0,0 +1,27 @@
+function login(){
+
+alert("Inicio de sesión correcto")
+
+window.location.href="alumno.html"
+
+}
+
+function registrar(){
+
+alert("Usuario registrado correctamente")
+
+window.location.href="login.html"
+
+}
+
+function irMapa(){
+
+window.location.href="mapa.html"
+
+}
+
+function irSolicitudes(){
+
+window.location.href="solicitudes.html"
+
+}
